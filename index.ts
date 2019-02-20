744f292ee3af22b177b448ca56f37b4e45fb63e1
Mark Borcherding <markborcherding@gmail.com>
2019-02-19 22:15:33 -0600

    move things to middleware

--- index.ts
+++ index.ts
@@ -4,22 +4,32 @@ import socketio from 'socket.io'
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio.listen(server)
+
+server.listen(3000, () => {
+  console.log('listening on *:3000')
+})
 
 
+// All of this is standard REST with no knowledge of sockets
 app.get('/', (req, res) => {
   console.log('get')
   res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/', (req, res) => {
-  io.emit("boom", {value: 'hi'})
+app.post('/', (req, res, next) => {
+  console.log('got it')
   res.send('OK')
+  next()
 })
 
-server.listen(3000, () => {
-  console.log('listening on *:3000')
+// we can wire up all this in a different module that only knows about sockety stuff
+const io = socketio.listen(server)
+app.post('/', (req, res, next) => {
+  console.log('telling everyone')
+  io.emit("boom", {some: 'value'})
+  next();
 })
+
 
 io.on('connection', (socket) => {
   console.log('a user connected');
